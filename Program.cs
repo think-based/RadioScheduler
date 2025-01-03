e71dd73ca476989ff3bc7738154f2c460fd15592
think-based <139097359+think-based@users.noreply.github.com>
2025-01-04 01:08:40 +1100

    Create new file via DeepSync Chrome Extension

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.ServiceProcess;
+
+namespace RadioSchedulerService
+{
+    static class Program
+    {
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new RadioSchedulerService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
