1a8bb9aa9fee13be686bb418bb689ece7322837e
think-based <139097359+think-based@users.noreply.github.com>
2025-01-06 05:15:26 +1100

    Update file via DeepSync Chrome Extension

--- Program.cs
+++ Program.cs
@@ -1,4 +1,21 @@
 //Be Naame Khoda
 //FileName: Program.cs
 
-using
+using System;
+using System.ServiceProcess;
+
+namespace RadioSchedulerService
+{
+    static class Program
+    {
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new RadioSchedulerService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
