194de8095aefe88a75526dc0191856e25e809c8e
think-based <139097359+think-based@users.noreply.github.com>
2025-01-05 22:52:39 +1100

    Create new file via DeepSync Chrome Extension

---
+++ FileHashHelper.cs
@@ -0,0 +1,20 @@
+//Be Naame Khoda
+//FileName: FileHashHelper.cs
+
+using System.Security.Cryptography;
+using System.Text;
+
+public static class FileHashHelper
+{
+    public static string CalculateFileHash(string filePath)
+    {
+        using (var md5 = MD5.Create())
+        {
+            using (var stream = File.OpenRead(filePath))
+            {
+                byte[] hashBytes = md5.ComputeHash(stream);
+                return BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant();
+            }
+        }
+    }
+}
