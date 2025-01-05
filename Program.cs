c8e3194b2c90498e2c2847fb4c916befcf8828d3
think-based <139097359+think-based@users.noreply.github.com>
2025-01-06 04:51:46 +1100

    Update file via DeepSync Chrome Extension

--- Program.cs
+++ Program.cs
@@ -1,20 +1,4 @@
 //Be Naame Khoda
 //FileName: Program.cs
 
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
-class Program
-{
-    static async Task Main(string[] args)
-    {
-        List<string> playlist = new List<string>
-        {
-            "audio1.mp3",
-            "audio2.mp3",
-            "audio3.mp3"
-        };
-
-        WebServer webServer = new WebServer(playlist);
-        await webServer.StartAsync
+using
