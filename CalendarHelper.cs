b398a28993f00bb95232d14a605f6ff55487aea1
think-based <139097359+think-based@users.noreply.github.com>
2025-01-06 04:45:19 +1100

    Update file via DeepSync Chrome Extension

--- CalendarHelper.cs
+++ CalendarHelper.cs
@@ -6,18 +6,4 @@ using System.Globalization;
 
 public static class CalendarHelper
 {
-    public static DateTime ConvertDate(DateTime date, string calendarType)
-    {
-        switch (calendarType)
-        {
-            case "Hijri":
-                HijriCalendar hijri = new HijriCalendar();
-                return new DateTime(hijri.GetYear(date), hijri.GetMonth(date), hijri.GetDayOfMonth(date));
-            case "Persian":
-                PersianCalendar persian = new PersianCalendar();
-                return new DateTime(persian.GetYear(date), persian.GetMonth(date), persian.GetDayOfMonth(date));
-            default: // Gregorian
-                return date;
-        }
-    }
-}
+    public static DateTime ConvertDate
