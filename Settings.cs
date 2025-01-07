d3edfe7b358c7b4848fec8693a40b19781bc91ca
think-based <139097359+think-based@users.noreply.github.com>
2025-01-08 06:08:35 +1100

    Update file via DeepSync Chrome Extension

--- Settings.cs
+++ Settings.cs
@@ -5,12 +5,12 @@ using System;
 
 public static class Settings
 {
-    // طول و عرض جغرافیایی
-    public static double Latitude { get; set; } = 35.6892; // عرض جغرافیایی پیش‌فرض (تهران)
-    public static double Longitude { get; set; } = 51.3890; // طول جغرافیایی پیش‌فرض (تهران)
+    // طول و عرض جغرافیایی (شیراز)
+    public static double Latitude { get; set; } = 29.5916; // عرض جغرافیایی شیراز
+    public static double Longitude { get; set; } = 52.5837; // طول جغرافیایی شیراز
 
-    // منطقه زمانی
-    public static double TimeZone { get; set; } = 3.5; // منطقه زمانی پیش‌فرض (تهران)
+    // منطقه زمانی (شیراز)
+    public static double TimeZone { get; set; } = 3.5; // منطقه زمانی شیراز
 
     // روش محاسبه
     public static PrayTime.CalculationMethod CalculationMethod { get; set; } = PrayTime.CalculationMethod.Tehran;
@@ -25,4 +25,32 @@ public static class Settings
 
     // تنظیمات مربوط به تایمر
     public static int TimerInterval { get; set; } = 24 * 60 * 60 * 1000; // تایمر پیش‌فرض (24 ساعت)
+
+    // زاویه‌های فجر و عشاء
+    private static double _fajrAngle = 18; // زاویه پیش‌فرض فجر
+    private static double _ishaAngle = 18; // زاویه پیش‌فرض عشاء
+
+    // متد تنظیم زاویه فجر
+    public static void setFajrAngle(double angle)
+    {
+        _fajrAngle = angle;
+    }
+
+    // متد تنظیم زاویه عشاء
+    public static void setIshaAngle(double angle)
+    {
+        _ishaAngle = angle;
+    }
+
+    // متد دریافت زاویه فجر
+    public static double getFajrAngle()
+    {
+        return _fajrAngle;
+    }
+
+    // متد دریافت زاویه عشاء
+    public static double getIshaAngle()
+    {
+        return _ishaAngle;
+    }
 }
