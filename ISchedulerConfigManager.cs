c0e047f60e39122ad814e74885f81eb1b43e8225
think-based <139097359+think-based@users.noreply.github.com>
2025-01-22 04:24:38 +1100

    Create new file via DeepSync Chrome Extension

---
+++ ISchedulerConfigManager.cs
@@ -0,0 +1,13 @@
+      //Be Naame Khoda
+//FileName: ISchedulerConfigManager.cs
+
+using System;
+using System.Collections.Generic;
+
+public interface ISchedulerConfigManager
+{
+    List<ScheduleItem> ScheduleItems { get; }
+    event Action ConfigReloaded;
+    void ReloadScheduleConfig();
+}
+    
