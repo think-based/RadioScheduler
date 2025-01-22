fc41ea3e2603ec1f82933cd6f5a4b1183ba6f2dd
think-based <139097359+think-based@users.noreply.github.com>
2025-01-23 01:01:58 +1100

    Update file via DeepSync Chrome Extension

--- CalendarHelper.cs
+++ CalendarHelper.cs
@@ -36,25 +36,14 @@ public static class CalendarHelper
         }
     }
 
-    public static DateTime ConvertToLocalTimeZone(DateTime dateTime, double timeZoneOffset, string region)
+    public static DateTime ConvertToLocalTimeZone(DateTime dateTime, string region)
     {
-        // Convert system time to the local time zone based on settings
-         TimeZoneInfo systemTimeZone = TimeZoneInfo.Local;
-        TimeZoneInfo targetTimeZone = TimeZoneInfo.CreateCustomTimeZone(
-            "PrayTimeZone",
-            TimeSpan.FromHours(timeZoneOffset),
-            "PrayTimeZone",
-            "PrayTimeZone");
+         // Convert system time to the local time zone based on settings
+        TimeZoneInfo systemTimeZone = TimeZoneInfo.Local;
+        TimeZoneInfo targetTimeZone = TimeZoneInfo.FindSystemTimeZoneById(Settings.TimeZoneId);
 
-        try
-        {
-            return TimeZoneInfo.ConvertTime(dateTime, systemTimeZone, targetTimeZone);
-        }
-        catch (TimeZoneNotFoundException)
-        {
-             Logger.LogMessage($"Invalid time zone with value {timeZoneOffset}.");
-            return dateTime;
-        }
+
+        return TimeZoneInfo.ConvertTime(dateTime, systemTimeZone, targetTimeZone);
     }
 }
     
