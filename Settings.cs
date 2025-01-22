87647a45a60a9b9df0bd75b5523843566cbd126d
think-based <139097359+think-based@users.noreply.github.com>
2025-01-23 00:56:03 +1100

    Update file via DeepSync Chrome Extension

--- Settings.cs
+++ Settings.cs
@@ -1,4 +1,4 @@
-//Be Naame Khoda
+      //Be Naame Khoda
 //FileName: Settings.cs
 
 using System;
@@ -8,7 +8,8 @@ public static class Settings
     // Static properties
     public static double Latitude { get; set; }
     public static double Longitude { get; set; }
-    public static double TimeZone { get; set; }
+    public static string TimeZoneId { get; set; }
+    public static double TimeZoneOffset { get; set; }
     public static PrayTime.CalculationMethod CalculationMethod { get; set; }
     public static PrayTime.AsrMethods AsrMethod { get; set; }
     public static PrayTime.TimeFormat TimeFormat { get; set; }
@@ -17,25 +18,28 @@ public static class Settings
     public static bool AmplifierEnabled { get; set; }
     public static string AmplifierApiUrl { get; set; }
 
-    // Angles for Fajr and Isha
-    private static double _fajrAngle;
+     private static double _fajrAngle;
     private static double _ishaAngle;
 
     public static double FajrAngle => _fajrAngle;
     public static double IshaAngle => _ishaAngle;
 
-    // Method to automatically set angles based on latitude
     public static void AutoSetAngles()
     {
-        if (Math.Abs(Latitude) <= 30) // Moderate regions
+        if (Math.Abs(Latitude) <= 30)
         {
-            _fajrAngle = 18; // Fajr angle
-            _ishaAngle = 18; // Isha angle
+            _fajrAngle = 18;
+            _ishaAngle = 18;
         }
-        else // High-latitude regions
+        else
         {
-            _fajrAngle = 17; // Fajr angle
-            _ishaAngle = 14; // Isha angle
+            _fajrAngle = 17;
+            _ishaAngle = 14;
         }
     }
+      public static double GetTimeZoneOffset()
+    {
+       return TimeZoneOffset;
+    }
 }
+    
